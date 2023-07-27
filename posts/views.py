242f82bf57a7338c8ee0ee1e3d4c6c680b7e7b60
rizwan-ai <rizwanse66@gmail.com>
2023-07-27 12:59:05 +0500

    Add Model

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+
+from django.http import HttpResponse
+
+
+# Function-based view
+def index(request):
+    return HttpResponse("Hello, world. You're at the posts index.")
